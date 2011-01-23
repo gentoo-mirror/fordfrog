4d67eef2e9c799527495231f3854266706588e10
Miroslav Šulc <fordfrog@gentoo.org>
2011-01-23 14:11:31 +0100

    updated the helper script so it works in git repo

--- scripts/netbeans/create_new_release.sh
+++ scripts/netbeans/create_new_release.sh
@@ -9,11 +9,10 @@ if [[ -z "${OLD}" ]] || [[ -z "${NEW}" ]] || [[ -z "${SOURCE_URL}" ]] ; then
 	exit 1
 fi
 
-pushd $(dirname $(readlink -f ${0})) >/dev/null || exit 1
-pushd $(hg root) >/dev/null || exit 1
+pushd $(dirname $(dirname $(dirname $(readlink -f ${0})))) >/dev/null || exit 1
 
 for file in */*/*${OLD}*.ebuild ; do
-	hg cp ${file} $(echo ${file} | sed "s/${OLD}/${NEW}/") || exit 1
+	cp ${file} $(echo ${file} | sed "s/${OLD}/${NEW}/") || exit 1
 done
 
 sed -i "s%SOURCE_URL=.*%SOURCE_URL=\"${SOURCE_URL}\"%" */*/*${NEW}*.ebuild || exit 1
@@ -22,5 +21,4 @@ for file in */*/*${NEW}*.ebuild ; do
 	ebuild ${file} manifest
 done
 
-popd >/dev/null || exit 1
 popd >/dev/null || exit 1
